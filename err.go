15e7b8b3f0035da04981dc67f0bbb7c83f5f3383
Amin Malekloo <amin.malekloo@yahoo.com>
2022-05-08 02:32:57 +0430

    feat: added error types

--- err.go
+++ err.go
@@ -1,1 +1,20 @@
 package stgin
+
+import "fmt"
+
+type ParseError struct {
+	tpe     string
+	details string
+}
+
+func (ije ParseError) Error() string {
+	return fmt.Sprintf("malformed %v context, %v", ije.tpe, ije.details)
+}
+
+type MalformedRequestContext struct {
+	details string
+}
+
+func (mrc MalformedRequestContext) Error() string {
+	return fmt.Sprintf("could not read from request, %v", mrc.details)
+}
