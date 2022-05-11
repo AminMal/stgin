27a7bf6d6b1044cd810cda6469a8a817a35315ed
AminMalekloo <amin.malekloo@yahoo.com>
2022-05-11 23:54:02 +0430

    feat: impl getting stackframe function for error recovery

--- init.go
+++ init.go
@@ -1,6 +1,10 @@
 package stgin
 
-import "github.com/AminMal/slogger"
+import (
+	"github.com/AminMal/slogger"
+	"runtime"
+	"strings"
+)
 
 var stginLogger slogger.Logger
 
@@ -9,4 +13,18 @@ var applicationJsonType = "application/json"
 
 func init() {
 	stginLogger = slogger.GetLogger("STGIN")
+}
+
+func relevantCaller() []runtime.Frame {
+	pc := make([]uintptr, 16)
+	n := runtime.Callers(1, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	var fs []runtime.Frame
+	for {
+		f, more := frames.Next()
+		if more && !strings.HasPrefix(f.Function, "github.com/AminMal/stgin") {
+			fs = append(fs, f)
+		} else { break }
+	}
+	return fs
 }
