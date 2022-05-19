08c32e974b6cf9e42542b390e9277dd200658e21
Amin Malekloo <amin.malekloo@yahoo.com>
2022-05-20 03:37:17 +0430

    added stautus godocs

--- status.go
+++ status.go
@@ -10,6 +10,8 @@ import (
 
 var emptyHeaders http.Header = make(map[string][]string, 5)
 
+// Status is the return type of stgin APIs.
+// It represents an HTTP response with a status code, headers, body and cookies.
 type Status struct {
 	StatusCode int
 	Entity     ResponseEntity
@@ -22,11 +24,13 @@ func (status Status) isRedirection() bool {
 	return status.StatusCode >= 300 && status.StatusCode < 400
 }
 
+// WithCookies returns a new Status, appended the given cookies.
 func (status Status) WithCookies(cookies ...*http.Cookie) Status {
 	status.cookies = append(status.cookies, cookies...)
 	return status
 }
 
+// WithHeaders returns a new Status, appended the given headers.
 func (status Status) WithHeaders(headers http.Header) Status {
 	for key, value := range headers {
 		status.Headers[key] = value
@@ -67,6 +71,8 @@ func (status Status) complete(request *http.Request, writer http.ResponseWriter)
 	}
 }
 
+// CreateResponse can be used in order to make responses that are not available in default functions in stgin.
+// For instance a 202 http response.
 func CreateResponse(statusCode int, body ResponseEntity) Status {
 	return Status{
 		StatusCode: statusCode,
@@ -77,10 +83,12 @@ func CreateResponse(statusCode int, body ResponseEntity) Status {
 
 // 2xx Statuses here
 
+// Ok represents a basic http 200 response with the given body.
 func Ok(body ResponseEntity) Status {
 	return CreateResponse(http.StatusOK, body)
 }
 
+// Created represents a basic http 201 response with the given body.
 func Created(body ResponseEntity) Status {
 	return CreateResponse(http.StatusCreated, body)
 }
@@ -88,6 +96,7 @@ func Created(body ResponseEntity) Status {
 // ------------------
 // 3xx statuses here
 
+// MovedPermanently represents a basic http 301 redirect to the given location.
 func MovedPermanently(location string) Status {
 	return Status{
 		StatusCode: http.StatusMovedPermanently,
@@ -95,6 +104,7 @@ func MovedPermanently(location string) Status {
 	}
 }
 
+// Found represents a basic http 302 redirect to the given location.
 func Found(location string) Status {
 	return Status{
 		StatusCode: http.StatusFound,
@@ -102,6 +112,7 @@ func Found(location string) Status {
 	}
 }
 
+// PermanentRedirect represents a basic http 308 redirect to the given location.
 func PermanentRedirect(location string) Status {
 	return Status{
 		StatusCode: http.StatusPermanentRedirect,
@@ -112,22 +123,27 @@ func PermanentRedirect(location string) Status {
 // ------------------
 // 4xx statuses here
 
+// BadRequest represents a basic http 400 response with the given body.
 func BadRequest(body ResponseEntity) Status {
 	return CreateResponse(http.StatusBadRequest, body)
 }
 
+// Unauthorized represents a basic http 401 response with the given body.
 func Unauthorized(body ResponseEntity) Status {
 	return CreateResponse(http.StatusUnauthorized, body)
 }
 
+// Forbidden represents a basic http 403 response with the given body.
 func Forbidden(body ResponseEntity) Status {
 	return CreateResponse(http.StatusForbidden, body)
 }
 
+// NotFound represents a basic http 404 response with the given body.
 func NotFound(body ResponseEntity) Status {
 	return CreateResponse(http.StatusNotFound, body)
 }
 
+// MethodNotAllowed represents a basic http 405 response with the given body.
 func MethodNotAllowed(body ResponseEntity) Status {
 	return CreateResponse(http.StatusMethodNotAllowed, body)
 }
@@ -135,12 +151,16 @@ func MethodNotAllowed(body ResponseEntity) Status {
 // ------------------
 // 5xx statuses here
 
+// InternalServerError represents a basic http 500 response with the given body.
 func InternalServerError(body ResponseEntity) Status {
 	return CreateResponse(http.StatusInternalServerError, body)
 }
 
 //------------------
 
+// File is used to return a file itself as an HTTP response.
+// If the file is not found, it returns 404 not found to the client.
+// If there are issues reading file or anything else related, 500 internal server error is returned to the client.
 func File(path string) Status {
 	file := fileContent{path}
 	_, err := file.Bytes()
