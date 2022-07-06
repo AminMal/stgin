1fd8bbd18e9c2b29df13f5091351be6309381936
AminMalekloo <amin.malekloo@yahoo.com>
2022-07-06 11:48:30 +0430

    added test case for route creation shorthands

--- route_test.go
+++ route_test.go
@@ -1,6 +1,7 @@
 package stgin
 
 import (
+	"net/http"
 	"testing"
 )
 
@@ -18,4 +19,49 @@ func TestMkRoute(t *testing.T) {
 func TestEmptyRouteCreationStage(t *testing.T) {
 	defer shouldHavePanicked(t)
 	OnPath("/test/$username").WithMethod("GET").Do(nil)
+}
+
+func TestGET(t *testing.T) {
+	helloAPI := func(RequestContext) Status {return Ok(Text("hello"))}
+	route := GET("/hello", helloAPI)
+	if route.Method != http.MethodGet {
+		t.Fatal("GET method could not set the correct http method")
+	}
+}
+
+func TestPUT(t *testing.T) {
+	helloAPI := func(RequestContext) Status {return Ok(Text("hello"))}
+	route := PUT("/hello", helloAPI)
+	if route.Method != http.MethodPut {
+		t.Fatal("GET method could not set the correct http method")
+	}
+}
+
+func TestPOST(t *testing.T) {
+	helloAPI := func(RequestContext) Status {return Ok(Text("hello"))}
+	route := POST("/hello", helloAPI)
+	if route.Method != http.MethodPost {
+		t.Fatal("GET method could not set the correct http method")
+	}
+}
+
+func TestPATCH(t *testing.T) {
+	helloAPI := func(RequestContext) Status {return Ok(Text("hello"))}
+	route := PATCH("/hello", helloAPI)
+	if route.Method != http.MethodPatch {
+		t.Fatal("GET method could not set the correct http method")
+	}
+}
+
+func TestDELETE(t *testing.T) {
+	helloAPI := func(RequestContext) Status {return Ok(Text("hello"))}
+	route := DELETE("/hello", helloAPI)
+	if route.Method != http.MethodDelete {
+		t.Fatal("GET method could not set the correct http method")
+	}
+}
+
+func TestMakeRouteNilAction(t *testing.T) {
+	defer shouldHavePanicked(t)
+	_ = mkRoute("/hello", nil, "DELETE")
 }
